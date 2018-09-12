663c79d74a991216a44bc2f85a4e76974c40b6d0
zhangruiyu <zhangruiyu@hexindai.com>
2018-09-12 22:12:35 +0800

    泛型添加

--- lib/banner.dart
+++ lib/banner.dart
@@ -3,29 +3,30 @@ import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-typedef void OnBannerClickListener(int index, dynamic itemData);
-typedef Widget BuildShowView(int index, dynamic itemData);
+typedef void OnBannerClickListener<D>(int index, D itemData);
+typedef Widget BuildShowView<D>(int index, D itemData);
 
 const IntegerMax = 0x7fffffff;
 
-class BannerView extends StatefulWidget {
-  final OnBannerClickListener onBannerClickListener;
+class BannerView<T> extends StatefulWidget {
+  final OnBannerClickListener<T> onBannerClickListener;
 
   //延迟多少秒进入下一页
   final int delayTime; //秒
   //滑动需要秒数
   final int scrollTime; //毫秒
   final double height;
-  final List data;
-  final BuildShowView buildShowView;
+  final List<T> data;
+  final BuildShowView<T> buildShowView;
 
-  BannerView({Key key,
-    @required this.data,
-    @required this.buildShowView,
-    this.onBannerClickListener,
-    this.delayTime = 3,
-    this.scrollTime = 200,
-    this.height = 200.0})
+  BannerView(
+      {Key key,
+      @required this.data,
+      @required this.buildShowView,
+      this.onBannerClickListener,
+      this.delayTime = 3,
+      this.scrollTime = 200,
+      this.height = 200.0})
       : super(key: key);
 
   @override
@@ -49,8 +50,8 @@ class BannerViewState extends State<BannerView> {
 
   resetTimer() {
     clearTimer();
-    timer = new Timer.periodic(
-        new Duration(seconds: widget.delayTime), (Timer timer) {
+    timer = new Timer.periodic(new Duration(seconds: widget.delayTime),
+        (Timer timer) {
       if (pageController.positions.isNotEmpty) {
         var i = pageController.page.toInt() + 1;
         pageController.animateToPage(i == 3 ? 0 : i,
@@ -69,46 +70,42 @@ class BannerViewState extends State<BannerView> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQueryData
-        .fromWindow(ui.window)
-        .size
-        .width;
+    double screenWidth = MediaQueryData.fromWindow(ui.window).size.width;
     return new SizedBox(
         height: widget.height,
         child: widget.data.length == 0
             ? null
-            :
-        new GestureDetector(
-          onTap: () {
+            : new GestureDetector(
+                onTap: () {
 //            print(pageController.page);
 //            print(pageController.page.round());
-            widget.onBannerClickListener(
-                pageController.page.round() % widget.data.length,
-                widget.data[
-                pageController.page.round() % widget.data.length]);
-          },
-          onTapDown: (details) {
+                  widget.onBannerClickListener(
+                      pageController.page.round() % widget.data.length,
+                      widget.data[
+                          pageController.page.round() % widget.data.length]);
+                },
+                onTapDown: (details) {
 //            print('onTapDown');
-            clearTimer();
-          },
-          onTapUp: (details) {
+                  clearTimer();
+                },
+                onTapUp: (details) {
 //            print('onTapUp');
-            resetTimer();
-          },
-          onTapCancel: () {
-            resetTimer();
-          },
-          child: new PageView.builder(
-            controller: pageController,
-            physics: const PageScrollPhysics(
-                parent: const ClampingScrollPhysics()),
-            itemBuilder: (BuildContext context, int index) {
-              return widget.buildShowView(
-                  index, widget.data[index % widget.data.length]);
-            },
-            itemCount: IntegerMax,
-          ),
-        ));
+                  resetTimer();
+                },
+                onTapCancel: () {
+                  resetTimer();
+                },
+                child: new PageView.builder(
+                  controller: pageController,
+                  physics: const PageScrollPhysics(
+                      parent: const ClampingScrollPhysics()),
+                  itemBuilder: (BuildContext context, int index) {
+                    return widget.buildShowView(
+                        index, widget.data[index % widget.data.length]);
+                  },
+                  itemCount: IntegerMax,
+                ),
+              ));
   }
 
   @override
